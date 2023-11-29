d193ccefec51f82cd0f403a83ef3d47cc97d3f3b
Martin Azpillaga Aldalur <martin@minim.tools>
2023-11-29 20:01:23 +0100

    3. Parsing the Code in the Editor

--- src/main/typescript/index.ts
+++ src/main/typescript/index.ts
@@ -1,3 +1,16 @@
 import * as monaco from "monaco-editor";
+import { CharStreams, CommonTokenStream } from "antlr4ts";
+import { JSONLexer } from "../antlr/JSONLexer";
+import { JSONParser } from "../antlr/JSONParser";
 
-monaco.editor.create(document.getElementById("editor")!, { theme: "vs-dark", fontSize: 22 });
+const editor = monaco.editor.create(document.getElementById("editor")!, { theme: "vs-dark", fontSize: 22 });
+
+editor.onDidChangeModelContent(() => {
+    const code = editor.getValue();
+    const characters = CharStreams.fromString(code);
+    const lexer = new JSONLexer(characters);
+    const tokens = new CommonTokenStream(lexer);
+    const parser = new JSONParser(tokens);
+    const tree = parser.json();
+    console.log(tree);
+});
